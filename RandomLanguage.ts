721425ef15b48be71418799fa5264eed1cd3b783
01KingMaker <fabrakoto0987@gmail.com>
2024-05-07 21:03:30 +0300

    initial commit

---
+++ RandomLanguage.ts
@@ -0,0 +1,46 @@
+import { isCode } from "./PattersonSardinass";
+
+import * as fs from "fs";
+
+function randomWord(min: number, max: number): string {
+    if (min > max) {
+        throw new Error("Min must not be greater than Max");
+    }
+    let randomLength = Math.floor( Math.random() * max + min);
+    let i = 0
+    let word = ""
+    while(i < randomLength) {
+        word += Math.floor(Math.random() * 2)
+        i ++;
+    }
+    return word;
+}
+
+function randomLanguage(min: number, max: number): string {
+    if (min > max) {
+        throw new Error("Min must not be greater than Max");
+    }
+    let randomLength = Math.floor(Math.random() * max + min);
+    let language: string[] = [];
+    let i = 0
+    while ( i < randomLength){
+        language.push(randomWord(1, 7))
+        i ++;
+    }
+    let strLanguage = language.toString()
+    if(isCode(language)) strLanguage = "1;" + strLanguage;
+    else strLanguage = "0;" + strLanguage
+    return strLanguage.replace(",", ";");
+}
+
+function generateMultipleLanguage(size: number, filePath: string): void{
+    let i = 0
+    let corpus = ""
+    while (i < size){
+        corpus += randomLanguage(1, 10) + "\n"
+        i ++;
+    }
+    fs.writeFileSync(filePath, corpus)
+}
+
+generateMultipleLanguage(10000, "./languages.csv")
